4401a99e5ec35609293950180fbf92ce03af641f
alanschlindvein <alansaraujo.schlindvein@gmail.com>
2018-06-27 13:43:16 -0300

    created build

--- src/commons/services/user.js
+++ src/commons/services/user.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { GH_API_URL } from 'commons/contants';
+import { GH_API_URL } from 'commons/constants';
 
 export default {
   getUser: user => axios.get(`${GH_API_URL}/users/${user}`)
